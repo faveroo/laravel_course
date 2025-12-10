a4b75f12375b6e1657a5a522ca47d9bfbb6fbe4e
Gabriel Favero <gabriel290974@gmail.com>
2025-12-09 22:46:31 -0300

    Adicionado o retorno do erro de login

--- note/resources/views/login.blade.php
+++ note/resources/views/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-6 col-sm-8">
             <div class="card p-5">
-                
+
                 <!-- logo -->
                 <div class="text-center p-3">
                     <img src="assets/images/logo.png" alt="Notes logo">
@@ -21,24 +21,30 @@
                                 <label for="text_username" class="form-label">Username</label>
                                 <input type="email" value="{{ old('text_username') }}" class="form-control bg-dark text-info" name="text_username" required>
                                 @error('text_username')
-                                    <div class="text-danger mt-1">
-                                        {{ $message }}
-                                    </div>
+                                <div class="text-danger mt-1">
+                                    {{ $message }}
+                                </div>
                                 @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="text_password" class="form-label">Password</label>
                                 <input type="password" value="{{ old('text_password') }}" class="form-control bg-dark text-info" name="text_password" required>
                                 @error('text_password')
-                                    <div class="text-danger mt-1">
-                                        {{ $message }}
-                                    </div>
+                                <div class="text-danger mt-1">
+                                    {{ $message }}
+                                </div>
                                 @enderror
                             </div>
                             <div class="mb-3">
                                 <button type="submit" class="btn btn-secondary w-100">LOGIN</button>
                             </div>
                         </form>
+
+                        @if (session('login_error'))
+                        <div class="alert alert-danger">
+                            {{ session('login_error') }}
+                        </div>
+                        @endif
                     </div>
                 </div>
 
