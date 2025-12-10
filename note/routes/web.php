339da78067d9da39c8f8391a5f292a5707ee40a1
Gabriel Favero <gabriel290974@gmail.com>
2025-12-09 23:25:21 -0300

    feat: Add name to root route

--- note/routes/web.php
+++ note/routes/web.php
@@ -5,6 +5,6 @@ use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('login');
-});
+})->name('index');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
