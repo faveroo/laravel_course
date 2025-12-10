528be4c610734efbb351b6aa9588040b89c264d0
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-12-10 15:19:03 -0300

    feat: implementação do middleware CheckIsNotLogged

--- note/routes/web.php
+++ note/routes/web.php
@@ -4,14 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Middleware\CheckIsLogged;
+use App\Http\Middleware\CheckIsNotLogged;
 
-Route::get('/login', function () {
-    return view('login');
-})->name('index');
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::middleware([CheckIsNotLogged::class])->group(function () {
+    Route::get('/login', [AuthController::class, 'index']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
 Route::middleware([CheckIsLogged::class])->group(function () {
-    Route::get('/', [MainController::class, 'index'])->name('home');
-    Route::post('/create-note', [MainController::class, 'createNote'])->name('create-note');
+    Route::get('/', [MainController::class, 'home'])->name('home');
+    Route::get('/create-note', [MainController::class, 'createNote'])->name('new');
     Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 });
