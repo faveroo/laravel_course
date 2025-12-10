5d048afce746580a3291d07104987b7a6040aa95
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-12-10 11:30:55 -0300

    Criação de um grupo para utilização do middleware de verificação

--- note/routes/web.php
+++ note/routes/web.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MainController;
+use App\Http\Middleware\CheckIsLogged;
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login');
 })->name('index');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware([CheckIsLogged::class])->group(function () {
+    Route::get('/', [MainController::class, 'index'])->name('home');
+    Route::post('/create-note', [MainController::class, 'createNote'])->name('create-note');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
