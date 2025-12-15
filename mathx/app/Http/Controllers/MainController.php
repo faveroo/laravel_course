1710489d2fdce2328fff0a16312e772bfde56256
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-12-15 17:21:05 -0300

    feat: Scaffold initial Laravel application with a main controller, basic routing, and UI assets.

---
+++ mathx/app/Http/Controllers/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function generateExercises(Request $request) {}
+
+    public function printExercises() {}
+
+    public function exportExercises() {}
+}
--- mathx/routes/web.php
+++ mathx/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
-Route::get('/', function () {
-    echo "MathX";
-});
+Route::get('/', [MainController::class, 'home'])->name('home');
+Route::post('/generate', [MainController::class, 'generateExercises'])->name('generate');
+Route::get('/print', [MainController::class, 'printExercises'])->name('print');
+Route::get('/export', [MainController::class, 'exportExercises'])->name('export');
