95efc353dc1414f318fd4e790e4c273b32da4ff0
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-12-16 12:12:53 -0300

    Feat: add exportExercises and printExercises methods

--- mathx/app/Http/Controllers/MainController.php
+++ mathx/app/Http/Controllers/MainController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\View\View;
+use SplFileObject;
 
 class MainController extends Controller
 {
     public function home(): View
     {
+        if (session()->has('exercises')) {
+            session()->forget('exercises');
+        }
         return view('home');
     }
 
@@ -39,52 +44,108 @@ class MainController extends Controller
         //generetion of exercises
         $exercises = [];
         for ($i = 1; $i <= $numberExercises; $i++) {
-            $operation = $operations[\array_rand($operations)];
-            $numberOne = \random_int($min, $max);
-            $numberTwo = \random_int($min, $max);
-
-            $exercise = '';
-            $sollution = '';
-
-            switch ($operation) {
-                case 'sum':
-                    $exercise = $numberOne . ' + ' . $numberTwo;
-                    $sollution = $numberOne + $numberTwo;
-                    break;
-                case 'subtraction':
-                    $exercise = $numberOne . ' - ' . $numberTwo;
-                    $sollution = $numberOne - $numberTwo;
-                    break;
-                case 'multiplication':
-                    $exercise = $numberOne . ' x ' . $numberTwo;
-                    $sollution = $numberOne * $numberTwo;
-                    break;
-                case 'division':
-
-                    if ($numberTwo == 0) {
-                        $numberTwo = 1;
-                    }
-
-                    $exercise = $numberOne . ' ÷ ' . $numberTwo;
-                    $sollution = $numberOne / $numberTwo;
-                    break;
-            }
-
-            if (is_float($sollution)) {
-                $sollution = number_format($sollution, 2);
-            }
-
-            $exercises[] = [
-                'exercise_number' => $i,
-                'exercise' => $exercise,
-                'sollution' => "$exercise = $sollution",
-            ];
+            $exercises[] = $this->generateExercise($i, $operations, $min, $max);
         }
+
+        session()->put('exercises', $exercises);
 
         return view('operations', compact('exercises'));
     }
 
-    public function printExercises() {}
+    public function printExercises()
+    {
+        if (!session()->has('exercises')) {
+            return redirect()->route('home');
+        }
 
-    public function exportExercises() {}
+        $exercises = session()->get('exercises');
+
+        echo '<pre>';
+        echo '<h1>Exercícios de matemática (' . env('APP_NAME') . ')</h1>';
+        echo '<hr>';
+
+        foreach ($exercises as $exercise) {
+            echo '<h2><small>' . $exercise['exercise_number'] . ' >> </small> ' . $exercise['exercise'] . '</h2>';
+        }
+
+        echo "<hr>";
+        echo "<h2>Respostas</h2>";
+
+        foreach ($exercises as $exercise) {
+            echo '<small>' . $exercise['exercise_number'] . ' >> ' . $exercise['sollution'] . '</small><br>';
+        }
+    }
+
+    public function exportExercises()
+    {
+        if (!session()->has('exercises')) {
+            return redirect()->route('home');
+        }
+
+        $exercises = session()->get('exercises');
+
+        // create download file
+        $filename = 'exercises_' . env('APP_NAME') . '_' . date('YmdHis') . '.txt';
+
+        $content = '';
+        foreach ($exercises as $exercise) {
+            $content .= $exercise['exercise_number'] . ' >> ' . $exercise['exercise'] . "\n";
+        }
+
+        $content .= "\n";
+        $content .= "Respostas\n" . str_repeat('=', 30) . "\n";
+        $content .= "\n";
+
+        foreach ($exercises as $exercise) {
+            $content .= $exercise['exercise_number'] . ' >> ' . $exercise['sollution'] . "\n";
+        }
+
+        return response($content)
+            ->header('Content-Type', 'text/plain')
+            ->header('Content-Disposition', 'attachment; filename="' . $filename . '"');
+    }
+
+    private function generateExercise($i, $operations, $min, $max)
+    {
+        $operation = $operations[\array_rand($operations)];
+        $numberOne = \random_int($min, $max);
+        $numberTwo = \random_int($min, $max);
+
+        $exercise = '';
+        $sollution = '';
+
+        switch ($operation) {
+            case 'sum':
+                $exercise = $numberOne . ' + ' . $numberTwo;
+                $sollution = $numberOne + $numberTwo;
+                break;
+            case 'subtraction':
+                $exercise = $numberOne . ' - ' . $numberTwo;
+                $sollution = $numberOne - $numberTwo;
+                break;
+            case 'multiplication':
+                $exercise = $numberOne . ' x ' . $numberTwo;
+                $sollution = $numberOne * $numberTwo;
+                break;
+            case 'division':
+
+                if ($numberTwo == 0) {
+                    $numberTwo = 1;
+                }
+
+                $exercise = $numberOne . ' ÷ ' . $numberTwo;
+                $sollution = $numberOne / $numberTwo;
+                break;
+        }
+
+        if (is_float($sollution)) {
+            $sollution = number_format($sollution, 2);
+        }
+
+        return [
+            'exercise_number' => str_pad($i, 2, '0', STR_PAD_LEFT),
+            'exercise' => $exercise,
+            'sollution' => "$exercise = $sollution",
+        ];
+    }
 }
