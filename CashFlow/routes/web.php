b4e70efc91b9b8b512232ba22b6f4c83cdbe39eb
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-12-17 13:48:40 -0300

    feat: add protected dashboard route with auth middleware

--- CashFlow/routes/web.php
+++ CashFlow/routes/web.php
@@ -2,7 +2,12 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\AuthMiddleware;
 
 Route::get('/', [AuthController::class, 'home'])->name('home');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
+
+Route::middleware(AuthMiddleware::class)->group(function () {
+    Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
+});
