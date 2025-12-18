28eac2c9ebb5a05f6ce6efc0b6219b3bbed352ec
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-12-18 11:21:22 -0300

    feat: add logout and transaction management routes

--- CashFlow/routes/web.php
+++ CashFlow/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\AuthMiddleware;
 
@@ -11,6 +12,8 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 
 Route::middleware(AuthMiddleware::class)->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/dashboard', [MainController::class, 'dashboard'])->name('dashboard');
     Route::get('/profile', [UserController::class, 'profile'])->name('profile');
+    Route::post('/transaction', [TransactionController::class, 'store'])->name('transaction.store');
 });
