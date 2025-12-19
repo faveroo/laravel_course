7c3f13e0832bad0381a35b421bf675b76d21ca53
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-12-19 16:44:15 -0300

    feat: add new render method

--- countries_and_capitals/app/Http/Controllers/MainController.php
+++ countries_and_capitals/app/Http/Controllers/MainController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
+    public function index()
+    {
+        return view('home');
+    }
+
     public function showData(
         CountryService $country
     ): JsonResponse {
