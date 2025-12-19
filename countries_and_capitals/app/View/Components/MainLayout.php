f3e4056f6143e8e4764767e0abaf5c9980ebd213
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-12-19 16:44:45 -0300

    refactor: fix the indentation

--- countries_and_capitals/app/View/Components/MainLayout.php
+++ countries_and_capitals/app/View/Components/MainLayout.php
@@ -11,10 +11,7 @@ class MainLayout extends Component
     /**
      * Create a new component instance.
      */
-    public function __construct()
-    {
-        //
-    }
+    public function __construct() {}
 
     /**
      * Get the view / contents that represent the component.
