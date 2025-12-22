4c34e360bd2cd7f4ce56934dbe53429ec93c06fd
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2025-12-22 10:26:05 -0300

    feat: load countries via service in constructor and add prepareGame method

--- countries_and_capitals/app/Http/Controllers/MainController.php
+++ countries_and_capitals/app/Http/Controllers/MainController.php
@@ -9,8 +9,38 @@ use Illuminate\View\View;
 
 class MainController extends Controller
 {
+    private $country_data;
+
+    public function __construct(
+        CountryService $country
+    ) {
+        $this->country_data = $country->all();
+    }
+
     public function startGame(): View
     {
         return view('home');
     }
+
+    public function prepareGame(Request $request)
+    {
+        $request->validate([
+            'total_questions' => ['required', 'min:3', 'max:30', 'integer']
+        ], [
+            'total_questions.required' => 'A quantidade de perguntas é obrigatória',
+            'total_questions.min' => 'A quantia mínima de perguntas é :min',
+            'total_questions.max' => 'A quantia máxima de perguntas é :max',
+            'total_questions.integer' => 'A quantia deve ser um número inteiro'
+        ]);
+
+        $total = intval($request->total_questions);
+
+        //quiz structure - prepare
+        $quiz = $this->prepareQuiz($total);
+
+        dd($quiz);
+    }
+
+
+    private function prepareQuiz(int $total) {}
 }
