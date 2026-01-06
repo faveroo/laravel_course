6e99380f634c77ff655138f4704a29bde1db82b5
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2026-01-06 15:47:29 -0300

    feat: Add new 'Nova Página' with its route, controller, and navigation, and seed an admin user.

--- LaravelBreeze/routes/web.php
+++ LaravelBreeze/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,4 +18,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::middleware(['auth'])->group(function () {
+    Route::get('/new-page', [MainController::class, 'newPage'])->name('new-page');
+});
+
+require __DIR__ . '/auth.php';
