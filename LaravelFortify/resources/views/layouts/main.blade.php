8247275a3ab3aea2bdb1def43bd4279545c5cad7
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2026-01-08 16:36:46 -0300

    utilizando metodos do blade para carregar os assets o title

--- LaravelFortify/resources/views/layouts/main.blade.php
+++ LaravelFortify/resources/views/layouts/main.blade.php
@@ -4,15 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
+    <title>{{ config('app.name') }}</title>
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.min.css') }}">
 </head>
 
 <body>
 
     @yield('content')
 
-    <script src="assets/bootstrap/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assets/bootstrap/bootstrap.bundle.min.js') }}"></script>
 </body>
 
 </html>
