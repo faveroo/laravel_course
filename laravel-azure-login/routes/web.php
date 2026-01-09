cb709deb94611d17601a6e08542b0c9272f1005a
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2026-01-09 09:43:05 -0300

    feat: add github authentication routes

--- laravel-azure-login/routes/web.php
+++ laravel-azure-login/routes/web.php
@@ -2,7 +2,17 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/auth/github', function () {
+    return Socialite::driver('github')->redirect();
+})->name('auth.github');
+
+Route::get('/auth/github/callback', function () {
+    $user = Socialite::driver('github')->user();
+    dd($user);
 });
