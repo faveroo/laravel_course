de727a0e1a175f6c5caeccf5bc41beac10b6076b
Gabriel Favero Hoffmann <gabriel290974@gmail.com>
2026-01-12 11:00:26 -0300

    feat: add custom error messages

--- LaravelFortify/app/Actions/Fortify/CreateNewUser.php
+++ LaravelFortify/app/Actions/Fortify/CreateNewUser.php
@@ -19,17 +19,30 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
-        Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => [
-                'required',
-                'string',
-                'email',
-                'max:255',
-                Rule::unique(User::class),
+        Validator::make(
+            $input,
+            [
+                'name' => ['required', 'string', 'max:255'],
+                'email' => [
+                    'required',
+                    'string',
+                    'email',
+                    'max:255',
+                    Rule::unique(User::class),
+                ],
+                'password' => $this->passwordRules(),
             ],
-            'password' => $this->passwordRules(),
-        ])->validate();
+            [
+                'name.required' => 'O campo nome é obrigatório',
+                'name.max' => 'O campo nome deve conter no máximo :max caracteres',
+                'email.required' => 'O campo email é obrigatório',
+                'email.email' => 'O campo email deve ser um email válido',
+                'email.unique' => 'O email informado já está em uso',
+                'password.required' => 'O campo de senha é obrigatório',
+                'password.min' => 'O campo de senha deve conter no mínimo :min caracteres',
+                'passowrd.confirmed' => 'O campo senha não confere com a confirmação de senha'
+            ]
+        )->validate();
 
         return User::create([
             'name' => $input['name'],
