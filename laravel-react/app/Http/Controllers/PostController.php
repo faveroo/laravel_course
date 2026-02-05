518b5f7c46cee15db4403f5dd41bbd47fdf9d22b
Gabriel Favero Hoffmann <74825744+faveroo@users.noreply.github.com>
2026-02-05 14:25:24 -0300

    inital commit

---
+++ laravel-react/app/Http/Controllers/PostController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Inertia::render('Index', [
+            'posts' => Post::all()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return Inertia::render('Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // We will make this function without any validation
+        // because this is simple CRUD example ¯\_(ツ)_/¯
+        Post::create([
+            'title' => $request->title,
+            'body' => $request->body
+        ]);
+
+        return redirect()->route('post.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+}
