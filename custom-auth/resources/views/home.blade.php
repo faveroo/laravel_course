96f53c050145f84915402374029fba9099ad242d
Gabriel Favero Hoffmann <74825744+faveroo@users.noreply.github.com>
2026-02-06 17:21:43 -0300

    feat: Add a new home page view with a welcome message.

--- custom-auth/resources/views/home.blade.php
+++ custom-auth/resources/views/home.blade.php
@@ -1,3 +1,12 @@
-<div>
-    <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
-</div>
+<x-layouts.main-layout :pageTitle="'Home'">
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-6">
+                <div class="card p-5">
+                    <h2 class="text-center mb-4">Home</h2>
+                    <p class="text-center">Bem-vindo ao sistema!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layouts.main-layout>
