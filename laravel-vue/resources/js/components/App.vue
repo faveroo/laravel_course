ae62f48ee121a4c40e0e3bb852d2735b91c3fdd1
Gabriel Favero Hoffmann <74825744+faveroo@users.noreply.github.com>
2026-02-09 13:56:06 -0300

    initial commit

---
+++ laravel-vue/resources/js/components/App.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1> Laravel + Vue </h1>
+        <Counter />
+    </div>
+</template>
+
+<script setup>
+import Counter from './Counter.vue';
+</script>
---
+++ laravel-vue/resources/js/components/Counter.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>Contador: {{ count }}</p>
+    <button @click="count++">+1</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
