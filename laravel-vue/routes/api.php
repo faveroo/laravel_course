99fcd6b8ec66ff5597142f2a5c3ff8dea44282c4
Gabriel Favero Hoffmann <74825744+faveroo@users.noreply.github.com>
2026-02-10 10:25:55 -0300

    feat: generate a random string for  /test-me api endpoint

--- laravel-vue/routes/api.php
+++ laravel-vue/routes/api.php
@@ -1,9 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Random\RandomError;
-use Random\RandomException;
+use Illuminate\Support\Str;
 
 Route::get("/test-me", function () {
-    return abort([402, 404, 500][array_rand([402, 404, 500])]);
+    return Str::random(10);
 });
