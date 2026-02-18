0dca40851928f6ece28b2c907f660680ada27bf9
Gabriel Favero Hoffmann <74825744+faveroo@users.noreply.github.com>
2026-02-18 14:54:45 -0300

    feat: change router-link to Link and update button and link style

--- laravel-vue/resources/js/Pages/HomeRoute.vue
+++ laravel-vue/resources/js/Pages/HomeRoute.vue
@@ -2,6 +2,7 @@
 
 import Header from '@/Components/Header.vue'
 import MainLayout from '@/Layouts/MainLayout.vue'
+import { Link } from '@inertiajs/vue3'
 
 defineOptions({
     layout: MainLayout,
@@ -43,11 +44,11 @@ const tiggerEndpoint = async () => {
 
 
 <template>
-    <div>
+    <div class="flex flex-col gap-4">
         <Header>Home</Header>
-        <router-link to="/test"> Take me to Test page </router-link>
+        <Link href="/test" class="text-black dark:text-white text-center p-4 bg-gray-300 dark:bg-gray-800 hover:bg-gray-400 dark:hover:bg-gray-900 hover:text-blue-800 dark:hover:text-blue-600 transition-colors"> Take me to Test page </Link>
         <button
-            class="bg-gray-800 dark:bg-gray-600 hover:bg-gray-700 dark:hover:bg-gray-500 text-white px-4 py-2 disabled:opacity-50 cursor-pointer"
+            class="bg-gray-300 dark:bg-gray-600 hover:bg-gray-400 dark:hover:bg-gray-500 text-black dark:text-white px-4 py-2 disabled:opacity-50 cursor-pointer"
             :disabled="loading"
             @click="tiggerEndpoint"
         >
