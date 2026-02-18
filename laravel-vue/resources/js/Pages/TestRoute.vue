cf95a6b91f60fb16fe78cbe241ad39aae6cbddce
Gabriel Favero Hoffmann <74825744+faveroo@users.noreply.github.com>
2026-02-18 14:57:21 -0300

    feat: Introduce a new `TestRoute` Vue page, add new web routes, and create initial dependency lock files for both PHP and Node.

--- laravel-vue/resources/js/Pages/TestRoute.vue
+++ laravel-vue/resources/js/Pages/TestRoute.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Header from '@/Components/Header.vue'
 import MainLayout from '@/Layouts/MainLayout.vue'
+import { Link } from '@inertiajs/vue3'
 
 defineOptions({
     layout: MainLayout,
@@ -10,4 +11,5 @@ defineOptions({
 <template>
     <Header>Test</Header>
     <p>I'm here to test!</p>
+    <Link href="/"> Take me to Home page </Link>
 </template>
